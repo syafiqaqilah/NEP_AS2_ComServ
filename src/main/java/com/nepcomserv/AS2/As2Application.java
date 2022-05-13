dfbc288690cdbcb48de75a6ecf6a8dd36b135034
syafiqaqilah <20ftt1415@student.pb.edu.bn>
2022-05-13 14:11:34 +0800

    Start point

---
+++ src/main/java/com/nepcomserv/AS2/As2Application.java
@@ -0,0 +1,13 @@
+package com.nepcomserv.AS2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class As2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(As2Application.class, args);
+	}
+
+}
