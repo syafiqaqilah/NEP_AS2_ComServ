8d8de593c412a255f6cf5d40c1b67d11e0c6159a
syafiqaqilah <20ftt1415@student.pb.edu.bn>
2022-05-14 20:33:01 +0800

    Added spring data jpa & msqlconnector

---
+++ src/main/java/com/nepcomserv/AS2/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.nepcomserv.AS2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value="/")
+	public String home() {
+		return "index";
+	}
+}
